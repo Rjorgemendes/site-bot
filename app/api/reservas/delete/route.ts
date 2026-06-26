5f22f96066d79fda0a29a8382317e7847d937de9
Jorge Mendes <rjorgemendes@sapo.pt>
2026-06-26 17:18:40 +0100

    remover rota delete antiga

--- app/api/reservas/delete/route.ts
+++
@@ -1,9 +0,0 @@
-import { reservas } from "../route";
-
-export async function POST(request: Request) {
-  const body = await request.json();
-
-  reservas.splice(body.index, 1);
-
-  return new Response(JSON.stringify({ ok: true }), { status: 200 });
-}
